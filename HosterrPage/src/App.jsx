2817914e5003628182ee7012108f2d97af694311
1anshu-stack <71838931+1anshu-stack@users.noreply.github.com>
2024-06-04 07:10:23 +0000

    mid

---
+++ HosterrPage/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Body from './components/Body'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-slate-400 min-h-screen px-6 py-3 flex flex-col gap-16'> 
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ HosterrPage/src/components/Body.jsx
@@ -0,0 +1,7 @@
+function Body(){
+    return (
+        <h1>body</h1>
+    )
+}
+
+export default Body
